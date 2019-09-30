0c1d7aae610ac1306c20d31cd32d15a062d8a51a
gurjeet <gurjit.babrah@gmail.com>
2019-09-30 10:20:46 -0400

    Initial commit

--- SpriteKitIntro/GameViewController.swift
+++ SpriteKitIntro/GameViewController.swift
@@ -15,31 +15,24 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+        
+        // initialize the initial game scene
+        let scene = GameScene(size:view.frame.size)
+        let skView = view as! SKView
+        
+        // add some debug info to the screen
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        // additional configuration options
+        
+        
+        // show the scene
+        skView.presentScene(scene)
+        
+
+
     }
 
     override var shouldAutorotate: Bool {
